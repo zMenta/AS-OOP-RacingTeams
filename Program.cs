3dbfc7ec039b030a1a7478027c7f36d27b28f97b
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-27 13:32:31 -0300

    AS | Program config

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using AS_OOP_RacingTeams.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(
+    x => x.UseSqlite()
+    // x => x.UseNpgsql() // Postgress
+);
 
 var app = builder.Build();
 
