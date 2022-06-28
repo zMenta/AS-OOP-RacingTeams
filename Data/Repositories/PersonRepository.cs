ddec6bb5282ecc0fecd12d5df5c67fd2fce73b8e
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-28 15:55:42 -0300

    AS | PersonRepository

--- Data/Repositories/PersonRepository.cs
+++ Data/Repositories/PersonRepository.cs
@@ -1,31 +1,54 @@
+using AS_OOP_RacingTeams.Data.Context;
 using AS_OOP_RacingTeams.Domain.Entities;
 using AS_OOP_RacingTeams.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace AS_OOP_RacingTeams.Data.Repositories
 {
-    public class PersonRepository : IJobRepository
+    public class PersonRepository : IPersonRepository
     {
+
+        private readonly DataContext _Context;
+
+        public PersonRepository(DataContext context)
+        {
+            this._Context = context;
+        }
+
         public bool Delete(int entityId)
         {
-            throw new NotImplementedException();
+            Person? Person = _Context.Persons.FirstOrDefault(i => i.Id == entityId);
+
+            if (Person == null)
+            {
+                return false;
+            }
+            else
+            {
+                _Context.Persons.Remove(Person);
+                return true;
+            }
         }
 
-        public Task<IList<Job>> GetAllAsync()
+
+        public async Task<IList<Person>> GetAllAsync()
+        {
+            return await _Context.Persons.ToListAsync();
+        }
+
+
+        public void Save(Person entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Job> GetByIdAsync(int entityId)
+
+        public void Update(Person entity)
         {
             throw new NotImplementedException();
         }
 
-        public void Save(Job entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Job entity)
+        Task<Person> IBaseRepository<Person>.GetByIdAsync(int entityId)
         {
             throw new NotImplementedException();
         }
