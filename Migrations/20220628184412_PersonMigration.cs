96cfc273b23e36fb2d4b38207514ca60cf199593
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-28 15:51:12 -0300

    AS | Person Migration

---
+++ Migrations/20220628184412_PersonMigration.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AS_OOP_RacingTeams.Migrations
+{
+    public partial class PersonMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "persons",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "VARCHAR", maxLength: 40, nullable: false),
+                    birth_year = table.Column<int>(type: "integer", nullable: false),
+                    job_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_persons", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_persons_jobs_job_id",
+                        column: x => x.job_id,
+                        principalTable: "jobs",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Cnpj = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonTeam",
+                columns: table => new
+                {
+                    PersonsId = table.Column<int>(type: "integer", nullable: false),
+                    TeamsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonTeam", x => new { x.PersonsId, x.TeamsId });
+                    table.ForeignKey(
+                        name: "FK_PersonTeam_persons_PersonsId",
+                        column: x => x.PersonsId,
+                        principalTable: "persons",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonTeam_Team_TeamsId",
+                        column: x => x.TeamsId,
+                        principalTable: "Team",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SponsorShip",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    TeamId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SponsorShip", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SponsorShip_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_persons_job_id",
+                table: "persons",
+                column: "job_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonTeam_TeamsId",
+                table: "PersonTeam",
+                column: "TeamsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SponsorShip_TeamId",
+                table: "SponsorShip",
+                column: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonTeam");
+
+            migrationBuilder.DropTable(
+                name: "SponsorShip");
+
+            migrationBuilder.DropTable(
+                name: "persons");
+
+            migrationBuilder.DropTable(
+                name: "Team");
+        }
+    }
+}
