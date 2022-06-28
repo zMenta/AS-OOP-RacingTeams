b8e7c6cf58546edbae770934bc06846e93ef059a
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-28 09:10:22 -0300

    AS | JobController

---
+++ Controllers/JobController.cs
@@ -0,0 +1,26 @@
+using AS_OOP_RacingTeams.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS_OOP_RacingTeams.Controllers
+{
+    [Route("api/")]
+    public class JobController : ControllerBase
+    {
+
+        private readonly IJobRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public JobController(IJobRepository repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var jobList = await _repository.GetAllAsync();
+            return Ok(jobList);
+        }
+    }
+}
