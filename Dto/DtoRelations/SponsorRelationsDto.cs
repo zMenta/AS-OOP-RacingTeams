c8063340a189298ed294ffe8a5e97ee319b40e6e
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-07-01 15:02:07 -0300

    AS | Removed "Teste prop"

--- Dto/DtoRelations/SponsorRelationsDto.cs
+++ Dto/DtoRelations/SponsorRelationsDto.cs
@@ -2,7 +2,7 @@ namespace AS_OOP_RacingTeams.Dto.DtoRelations
 {
     public class SponsorRelationsDto
     {
-         public int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
